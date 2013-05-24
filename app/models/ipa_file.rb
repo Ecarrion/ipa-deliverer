d97de8f54e219f50462e4e2829d2ce8e93fcb244
Ernesto Carrión <ernesto@kogimobile.com>
2013-05-24 10:24:07 -0500

    index method

--- app/models/ipa_file.rb
+++ app/models/ipa_file.rb
@@ -2,7 +2,16 @@ class IpaFile < ActiveRecord::Base
   # attr_accessible :title, :body
 
   def self.save(upload)
-  	
+
+  	name = upload['ipafile'].original_filename
+  	directory = "public/data"
+
+	#create the file path
+	path = File.join(directory, name)
+
+	#write the file
+	File.open(path, "wb") { |file| file.write(upload['ipafile'].read) }
+
   end
 
 end
