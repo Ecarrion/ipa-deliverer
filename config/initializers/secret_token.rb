9ac7aa33c394c1cc328a297de065a2861d032bbc
Ernesto Carrión <ernesto@kogimobile.com>
2013-05-24 08:50:35 -0500

    initial import

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+IpaDeliverer::Application.config.secret_token = '021fe4b6847261394648a3c41586b2c5566dd27df79c72e7737fadb720995d2779de9e6340036b7888b616b5463a72362e386347788318ca2b5fd71c9cb02dc1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+IpaDeliverer::Application.config.session_store :cookie_store, key: '_ipa_deliverer_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# IpaDeliverer::Application.config.session_store :active_record_store
